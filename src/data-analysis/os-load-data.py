58617a95eb27e6bc07dce5deb45a514aa84a77f5
Scott Wakefield <scott@expertcc.co.uk>
2024-07-29 10:58:23 +0100

    MEESALPHA-80 os download working better

--- src/data-analysis/os-load-data.py
+++ src/data-analysis/os-load-data.py
@@ -1,34 +1,59 @@
 import urllib.request
 from urllib.parse import urlencode
+
+import requests
 from dotenv import load_dotenv
 import os
-import progressbar
+from tqdm import tqdm
 
 load_dotenv()
 
 token = os.getenv('OS_TOKEN')
-output_file = os.getenv('OS_OUTPUT_FILE')
+output_file = os.getenv('OS_FILENAME')
 base_url = os.getenv('OS_BASE_URL')
 full_url = f'{base_url}{token}'
-pbar = None
 
 
-def show_progress(block_num, block_size, total_size):
-    global pbar
-    if pbar is None:
-        pbar = progressbar.ProgressBar(maxval=total_size)
-        pbar.start()
+# def show_progress(block_num, block_size, total_size):
+#     global pbar
+#     if pbar is None:
+#         pbar = progressbar.ProgressBar(maxval=total_size)
+#         pbar.start()
+#
+#     downloaded = block_num * block_size
+#     if downloaded < total_size:
+#         pbar.update(downloaded)
+#     else:
+#         pbar.finish()
+#         pbar = None
 
-    downloaded = block_num * block_size
-    if downloaded < total_size:
-        pbar.update(downloaded)
-    else:
-        pbar.finish()
-        pbar = None
-
-
-opener = urllib.request.build_opener()
-urllib.request.install_opener(opener)
-urllib.request.urlretrieve(full_url, output_file, show_progress)
+#
+# opener = urllib.request.build_opener()
+# urllib.request.install_opener(opener)
+# urllib.request.urlretrieve(full_url, output_file, show_progress)
 # 14% after 2hrs
+
+
+def download_file(url, file_name):
+    file_open_mode = 'wb'
+    if os.path.exists(file_name):
+        file_open_mode = 'ab'
+        # and resume from, like pycurls c.setopt(c.RESUME_FROM, os.path.getsize(file_name))
+        # create a resume_header and pass that to requests.get() - resume_header= {'Range': 'bytes=%d-' % resume_byte_pos}
+    with requests.get(url, stream=True) as r:
+        total_size = int(r.headers.get('content-length', 0))
+        block_size = 1024 * 8
+        r.raise_for_status()
+        with tqdm(total=total_size, unit='B', unit_scale=True, unit_divisor=block_size) as pbar:
+            with open(file_name, file_open_mode) as f:
+                for chunk in r.iter_content(chunk_size=block_size):
+                    pbar.update(len(chunk))
+                    f.write(chunk)
+        if total_size != 0 and pbar.n != total_size:
+            raise RuntimeError("Failed to download file")
+
+    return
+
+
+download_file(full_url, output_file)
 #%%

42414a725085fee50e618cedb80756ff87d09981
Scott Wakefield <scott@expertcc.co.uk>
2024-09-07 09:23:36 +0100

    MEESALPHA-146 further changes to get a data proj running

---
+++ src/data-tool/Data/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace Desnz.Mees.DataTool.Data;
+
+public class Class1
+{
+    /* get new postgres docker - done
+     * try docker compose - done
+     * gitignore for .net
+     * create db
+     * set up types
+     * dbcontext
+     * figure out how to deploy to aws
+     */
+}

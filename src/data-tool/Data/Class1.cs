008b045326e2c2a8643e70c8d551dc5a0a919537
Scott Wakefield <scott@expertcc.co.uk>
2024-09-08 20:11:22 +0100

    MEESALPHA-146 fixed port issues in compose, setup secrets

--- src/data-tool/Data/Class1.cs
+++ src/data-tool/Data/Class1.cs
@@ -4,10 +4,15 @@ public class Class1
 {
     /* get new postgres docker - done
      * try docker compose - done
-     * gitignore for .net
+     * gitignore for .net - done
      * create db
      * set up types
      * dbcontext
      * figure out how to deploy to aws
      */
+}
+
+public class DataSettings
+{
+    public string DbConnectionString { get; set; }
 }

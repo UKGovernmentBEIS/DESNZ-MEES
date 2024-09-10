176ee45b05eac87a1338290f1cca03397e22756a
Scott Wakefield <scott@expertcc.co.uk>
2024-09-10 14:14:59 +0100

    MEESALPHA-146 mod voa to fit aodc, able to import all parquets

--- src/data-tool/Data/Class1.cs
+++ src/data-tool/Data/Class1.cs
@@ -6,10 +6,16 @@ public class Class1
      * try docker compose - done
      * gitignore for .net - done
      * create db - done
-     * set up types
+     *
+     * give up on aws for now. can't create lambdas which are needed for data import to rds and db api
+     * filter abp by commercial only - smaller files, xref - only with uarns.
+     * recreate db and load in tables.
+     * web app, api, db, domain proj. all running locally
+     * 
+     * set up types, 
      * dbcontext
-     * figure out how to deploy to aws
-     * can write from parquet to postgres with python script, need to get it all on aws.
+     * figure out how to deploy to aws 
+     * can write from parquet to postgres with python script in lambda, need to get it all on aws.
      */
 }
 
